daf3f334975ae539dcbd718c08057cb6c2eb3d5b
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2022-09-23 12:58:36 +0200

    EOF

--- controller/Products.ts
+++ controller/Products.ts
@@ -1,6 +1,50 @@
+import Product from "../model/Product";
 import { Controller } from "./Controller";
+import { v4 as uuidv4 } from 'uuid'
 
-export const read: Controller = async (req, res) => {
+interface productData {
+    brand: string
+    category: string
+    subCategory: string
+    modelName: string
+    shortDescription: string
+    longDescription: string
+    color: string
+    size: string
+    pictureFile: string
+    netPrice: string
+}
+
+interface productDataCollected extends Array<productData> { }
+
+export const createProducts: Controller = async (req, res) => {
     const { data } = req.body
     res.json(data)
+    if (!data) {
+        res.status(500).json({ message: "Invalid arguments" })
+    }
+    const refinedData = JSON.parse(data) as productDataCollected
+    try {
+        refinedData.map(p => createProduct(p))
+    } catch (err) {
+        res.status(500).json({ message: (err as Error).message })
+    }
+}
+
+const createProduct = ({ brand, category, subCategory,
+    modelName, shortDescription, longDescription,
+    color, size, pictureFile, netPrice }: productData) => {
+    Product.create({
+        id: uuidv4(),
+        brand,
+        category,
+        subCategory,
+        modelName,
+        shortDescription,
+        longDescription,
+        color,
+        size,
+        pictureFile,
+        netPrice
+    })
 }
--- routes.ts
+++ routes.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { authenticate, authenticateLogin } from "./controller/Authenticate"
+import { createProducts } from "./controller/Products"
 import { createUser, getUserById, getUsers } from "./controller/User"
 
 export const router = express.Router()
@@ -9,3 +10,4 @@ router.get('/users', getUsers)
 router.get('/user/:id', getUserById)
 router.post('/signup', createUser)
 router.post('/authenticate', authenticateLogin)
+router.post('/read', createProducts)
